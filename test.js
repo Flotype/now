2f42cad0f796c2e17db3c2a509a3a3ad0fabcaa3
ericz <really.ez@gmail.com>
2011-03-02 21:04:28 -0800

    asdf

--- test.js
+++ test.js
@@ -16,14 +16,7 @@ var now = {};
 now = proxy.wrap(store, now);
 
 setTimeout(function(){
-    now.a = {c: [{}]};
-    now.b = now.a.c[0];
-    
-    var stringified_now = JSON.stringify(now);
-    console.log(stringified_now);
-    var restored_now = cycle.retrocycle(JSON.parse(stringified_now));
-    console.log(JSON.stringify(restored_now));
-    console.log(restored_now.b == restored_now.a.c[0]);
+
 }, 2000);
 
 
