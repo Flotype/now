3303fee549e0b1a69a8e728075130a94515094b1
ericz <really.ez@gmail.com>
2011-03-07 12:28:48 -0800

    example added

---
+++ examples/helloworld_server.js
@@ -0,0 +1,18 @@
+﻿
+var fs = require('fs');
+var server = require('http').createServer(function(req, response){
+  fs.readFile('helloworld.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'});  
+    response.write(data);  
+    response.end();
+  });
+});
+server.listen(8080);
+
+var everyone = require("../lib/nowServerLib.js").initialize(server);
+
+
+
+
+
+everyone.now.distributeMessage = [function(name, message){everyone.now.receiveMessage(name, message);}];
