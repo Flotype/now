0458a3cb7202c317f7ae2525aa67e8db4361b0cc
ericz <really.ez@gmail.com>
2011-03-26 12:35:48 -0700

    fixing typeof `object` checks, thanks dvv

--- examples/helloworld_example/helloworld_server.js
+++ examples/helloworld_example/helloworld_server.js
@@ -1,7 +1,7 @@
 ﻿var fs = require('fs');
 var server = require('http').createServer(function(req, response){
   fs.readFile('helloworld.html', function(err, data){
-    response.writeHead(200, {'Content-Type':'text/html'});  
+    response.writeHead(200, {'Content-Type':'text/html'}); 
     response.write(data);  
     response.end();
   });
