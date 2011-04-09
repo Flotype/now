ecfdc0922301e4c32e03e7084c68a107fe780f59
Eric Zhang <really.ez@gmail.com>
2011-04-08 21:26:40 -0700

    added multiroom chat, fixed extra event call (thanks dvv), removed extraneous console.logs

---
+++ examples/multiroomchat_example/multiroomchat_server.js
@@ -0,0 +1,36 @@
+﻿var fs = require('fs');
+var server = require('http').createServer(function(req, response){
+  fs.readFile(__dirname+'/multiroomchat.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'}); 
+    response.write(data);  
+    response.end();
+  });
+});
+server.listen(8080);
+
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+
+everyone.on('connect', function(){
+  this.now.room = "room 1";
+  nowjs.getGroup(this.now.room).addUser(this.user.clientId);
+  console.log("Joined: " + this.now.name);
+});
+
+
+everyone.on('disconnect', function(){
+  console.log("Left: " + this.now.name);
+});
+
+everyone.now.changeRoom = function(newRoom){
+  nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
+  nowjs.getGroup(newRoom).addUser(this.user.clientId);
+  this.now.room = newRoom;
+  this.now.receiveMessage("SERVER", "You're now in " + this.now.room);
+}
+
+everyone.now.distributeMessage = function(message){
+  nowjs.getGroup(this.now.room).now.receiveMessage(this.now.name, message);
+};
