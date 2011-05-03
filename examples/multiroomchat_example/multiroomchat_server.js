f11d59f1fabeefd4776bfabe684900bb9ac97b15
AD7six <andydawson76@gmail.com>
2011-05-03 11:37:15 +0200

    don't send messages back to the user

--- examples/multiroomchat_example/multiroomchat_server.js
+++ examples/multiroomchat_example/multiroomchat_server.js
@@ -14,8 +14,16 @@ var everyone = nowjs.initialize(server);
 
 
 everyone.on('connect', function(){
-  this.now.room = "room 1";
-  nowjs.getGroup(this.now.room).addUser(this.user.clientId);
+  var defaultRoom = "room 1",
+      group = nowjs.getGroup(defaultRoom);
+
+  this.now.room = defaultRoom;
+
+  if (group.distributeMessage) {
+    group.distributeMessage(this.now.name + " has joined the channel");
+  }
+  group.addUser(this.user.clientId);
+
   console.log("Joined: " + this.now.name);
 });
 
@@ -32,5 +40,12 @@ everyone.now.changeRoom = function(newRoom){
 }
 
 everyone.now.distributeMessage = function(message){
-  nowjs.getGroup(this.now.room).now.receiveMessage(this.now.name, message);
+  nowjs.getGroup(this.now.room).now.filterMessage(this.user.clientId, this.now.name, message);
 };
+
+everyone.now.filterMessage = function(clientId, name, message){
+    if (clientId === this.user.clientId) {
+        return;
+    }
+    this.now.receiveMessage(name, message);
+}
