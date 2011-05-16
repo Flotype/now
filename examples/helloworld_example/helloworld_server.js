4542fced969cb80dd98282cf8a2908f1f830aa1e
Darshan Shankar <D.n.Shankar@gmail.com>
2011-05-16 13:52:02 -0700

    updating docs

--- examples/helloworld_example/helloworld_server.js
+++ examples/helloworld_example/helloworld_server.js
@@ -1,4 +1,4 @@
-﻿var fs = require('fs');
+var fs = require('fs');
 var server = require('http').createServer(function(req, response){
   fs.readFile(__dirname+'/helloworld.html', function(err, data){
     response.writeHead(200, {'Content-Type':'text/html'}); 
@@ -7,16 +7,17 @@ var server = require('http').createServer(function(req, response){
   });
 });
 server.listen(8080);
-var everyone = require("now").initialize(server);
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
 
-
-everyone.connected(function(){
+everyone.on("connect", function(){
   console.log("Joined: " + this.now.name);
 });
 
-
-everyone.disconnected(function(){
+everyone.on("disconnect", function(){
   console.log("Left: " + this.now.name);
 });
 
-everyone.now.distributeMessage = function(message){everyone.now.receiveMessage(this.now.name, message);};
+everyone.now.distributeMessage = function(message){
+  everyone.now.receiveMessage(this.now.name, message);
+};
