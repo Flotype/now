Flotype/now

commit 8fa022692ba8d243869333136f2f778862a6637e

convert data.args in remote calls to array if necessary. GH #86

lib/now.js
@@ -37,9 +37,18 @@ var nowCore = {
       }
       
       var theArgs = data.args;
+
+      if(typeof theArgs === "object" && !nowUtil.isArray(theArgs)) {
+        var args = [];
+        // Enumeration order is not defined so this might be useless, but there will be cases when it works
+        for(var i in theArgs) {
+          args.push[theArgs[i]];
+        }
+        theArgs = args;
+      }
       
       // Search (only at top level) of args for functions parameters, and replace with wrapper remote call function
-      for(var i in theArgs){
+      for(var i, ii = theArgs.length; i < ii; i++){
         if(Object.hasOwnProperty.call(theArgs[i], 'type') && theArgs[i].type === 'function'){
           theArgs[i] = nowCore.constructRemoteFunction(client, theArgs[i].fqn);
         }

lib/nowServerLib.js
@@ -65,8 +65,19 @@ var nowCore = {
       
       var theArgs = data.args;
       
+      // Convert object to array if for some reason it was sent incorrectly
+      // This should rarely happen
+      if(typeof theArgs === "object" && !nowUtil.isArray(theArgs)) {
+        var args = [];
+        // Enumeration order is not defined so this might be useless, but there will be cases when it works
+        for(var i in theArgs) {
+          args.push[theArgs[i]];
+        }
+        theArgs = args;
+      }
+      
       // Search (only at top level) of args for functions parameters, and replace with wrapper remote call function
-      for(var i in theArgs){
+      for(var i, ii = theArgs.length; i < ii; i++){
         if(Object.hasOwnProperty.call(theArgs[i], 'type') && theArgs[i].type === 'function'){
           theArgs[i] = nowCore.constructRemoteFunction(client, theArgs[i].fqn);
         }

commit 13dea00a8484bbc8c3b04f93808c6369c2e0828a

fixed callbacks not working #96, regression from 8fa022692ba8d243869333136f2f778862a6637e

lib/now.js
@@ -48,7 +48,7 @@ var nowCore = {
       }
 
       // Search (only at top level) of args for functions parameters, and replace with wrapper remote call function
-      for(var i, ii = theArgs.length; i < ii; i++){
+      for(var i = 0, ii = theArgs.length; i < ii; i++){
         if(nowUtil.hasProperty(theArgs[i], 'type') && theArgs[i].type === 'function'){
           theArgs[i] = nowCore.constructRemoteFunction(client, theArgs[i].fqn);
         }

lib/nowServerLib.js
@@ -77,7 +77,7 @@ var nowCore = {
       }
       
       // Search (only at top level) of args for functions parameters, and replace with wrapper remote call function
-      for(var i, ii = theArgs.length; i < ii; i++){
+      for(var i = 0, ii = theArgs.length; i < ii; i++){
         if(nowUtil.hasProperty(theArgs[i], 'type') && theArgs[i].type === 'function'){
           theArgs[i] = nowCore.constructRemoteFunction(client, theArgs[i].fqn);
         }