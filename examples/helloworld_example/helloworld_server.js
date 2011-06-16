a605b31e2336757daa67b3da489e15ec3c573442
ericz <really.ez@gmail.com>
2011-06-15 21:57:34 -0700

    first commit

---
+++ examples/helloworld_example/helloworld_server.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var server = require('http').createServer(function(req, response){
+  fs.readFile(__dirname+'/helloworld.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'}); 
+    response.write(data);  
+    response.end();
+  });
+});
+server.listen(8080);
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+everyone.on("connect", function(){
+  console.log("Joined: " + this.now.name);
+});
+
+everyone.on("disconnect", function(){
+  console.log("Left: " + this.now.name);
+});
+
+everyone.now.distributeMessage = function(message){
+  everyone.now.receiveMessage(this.now.name, message);
+};
