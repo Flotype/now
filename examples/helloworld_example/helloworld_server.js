04c378caec604250446ab639b1ef4bb618debc18
Steve Wang <steve.john.wang@gmail.com>
2011-11-23 17:52:14 -0800

    Preliminary session support; not currently working.

--- examples/helloworld_example/helloworld_server.js
+++ examples/helloworld_example/helloworld_server.js
@@ -1,16 +1,20 @@
 var fs = require('fs');
-var server = require('http').createServer(function(req, response){
-  fs.readFile(__dirname+'/helloworld.html', function(err, data){
-    response.writeHead(200, {'Content-Type':'text/html'});
-    response.write(data);
-    response.end();
-  });
+var express = require('express');
+var server = express.createServer();
+console.log('blargh');
+server.use(express.cookieParser());
+var store = {store: null};
+server.use(express.session({ secret: "keyboard cat" }));
+server.get('/', function (req, res) {
+  res.contentType(__dirname + '/helloworld.html');
+  res.send(fs.readFileSync(__dirname + '/helloworld.html'));
+  store.store = req.sessionStore;
+  console.log('This got read.');
+  console.log(store.store);
 });
 server.listen(8080);
 var nowjs = require("now");
-var everyone = nowjs.initialize(server);
-
-
+var everyone = nowjs.initialize(server, {connect: store});
 
 nowjs.on("connect", function(){
   console.log("Joined: " + this.now.name);
@@ -20,7 +24,9 @@ nowjs.on("disconnect", function(){
   console.log("Left: " + this.now.name);
 });
 
-
 everyone.now.distributeMessage = function(message){
+  console.log(store);
+  console.log(this.user.cookie);
+  console.log(this.user.session);
   everyone.now.receiveMessage(this.now.name, message);
 };
